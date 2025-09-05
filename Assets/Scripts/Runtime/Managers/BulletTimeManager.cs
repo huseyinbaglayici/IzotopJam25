e584c484e1d451e69157681ad0e2864d8a07372a
huseyinbaglayici <huseyinow35@hotmail.com>
2025-09-05 23:45:59 +0300

    bullettimeinit

---
+++ Assets/Scripts/Runtime/Managers/BulletTimeManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Runtime.Extension;
+using UnityEngine;
+
+namespace Runtime.Managers
+{
+    public class BulletTimeManager : MonoSingleton<BulletTimeManager>
+    {
+        [Header("Bullet Time Manager")]
+        public float slowTimeScale = 0.2f;
+        public float duration = 2f;
+        public float transitionSpeed = 5f;
+
+        private float normalTimeScale = 1f;
+        private float timer;
+        private bool isBulletTime;
+
+
+        private void Update()
+        {
+            if (isBulletTime)
+            {
+                timer -= Time.unscaledTime;
+                if (timer <= 0f)
+                {
+                    EndBulletTime();
+                }
+            }
+            
+            //Smooth switch
+            Time.timeScale = Mathf.Lerp(Time.timeScale,isBulletTime ? slowTimeScale : normalTimeScale,
+                Time.unscaledDeltaTime * transitionSpeed);
+            Time.fixedDeltaTime = 0.02f * Time.timeScale;
+        }
+
+        public void StartBulletTime()
+        {
+            isBulletTime = true;
+            timer = duration;
+        }
+
+        private void EndBulletTime()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
